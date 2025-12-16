bc314c3307dbaba97264df1005a48c8c5c2f5eea
sunzhaoc <suznahc_2011@163.com>
2025-12-17 00:31:15 +0800

    2025-12-17 00:31:07

--- auto_update_git_shell.sh
+++ auto_update_git_shell.sh
@@ -6,7 +6,11 @@ if [ $# -ge 1 ]; then commit_msg="$date $1"
 else commit_msg="$date"
 fi
 
-# echo "Submit info is: $commit_msg"
+# echo "Submit info is: $commit_msg
+
+go mod tidy
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o plant_be ./cmd/plant_be/main.go
+upx -9 plant_be
 
 git add .
 git commit -m "$commit_msg" 
