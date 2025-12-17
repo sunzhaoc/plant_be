4be71524c5749b0f677f0bd8e611f4deb30d08ec
sunzhaoc <suznahc_2011@163.com>
2025-12-17 11:01:08 +0800

    1

--- cmd/plant_be/main.go
+++ cmd/plant_be/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"log"
+
+	"github.com/sunzhaoc/plant_be/pkg/db/mysql"
 	"github.com/sunzhaoc/plant_be/routers"
 )
 
 func main() {
+	if err := mysql.Init(mysql.Load(), []string{"ali"}); err != nil {
+		log.Fatal("初始化数据库失败：%v", err)
+	}
+	defer mysql.Close()
+
 	routers.InitRouter()
 }
