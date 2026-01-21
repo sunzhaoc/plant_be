f40f34e51beda648aeb7c47a855b4642524f2f6c
sunzhaoc <suznahc_2011@163.com>
2026-01-21 18:30:45 +0800

    2026-01-21 18:30:35

--- cmd/plant_be/main.go
+++ cmd/plant_be/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/sunzhaoc/plant_be/pkg/db/mysql"
+	"github.com/sunzhaoc/plant_be/pkg/db/redis"
 	"github.com/sunzhaoc/plant_be/routers"
 )
 
@@ -15,9 +16,9 @@ func main() {
 	defer mysql.Close()
 
 	// 初始化 Redis
-	//if err := redis.Init(redis.Load(), []string{"ali"}); err != nil {
-	//	log.Fatalf("初始化Redis数据库失败：%v", err)
-	//}
+	if err := redis.Init(redis.Load(), []string{"ali"}); err != nil {
+		log.Fatalf("初始化Redis数据库失败：%v", err)
+	}
 
 	routers.InitRouter()
 }
